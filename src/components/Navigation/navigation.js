c74a81ea6cf955cf41804f723f85c49dd3c92357
Mahmuda <mahmuda2709@gmail.com>
2022-03-17 14:38:13 -0600

    added

---
+++ src/components/Navigation/navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './navigation.css'
+
+const navigation = () => {
+    return (
+        <nav style = {{display: 'flex', justifyContent: 'flex-end'}}>
+        <p className="navstyle">Sign Out</p>
+        </nav>
+    );
+}
+
+export default navigation;
