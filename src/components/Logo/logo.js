0683aa1a68a34abca1e5e236f43ad0cd7cfc7013
Mahmuda <mahmuda2709@gmail.com>
2022-03-17 15:23:23 -0600

    adding logo

--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Tilt from 'react-tilt';
+import './logo.css';
+import teddy from './teddy.png';
 
 const logo = () => {
     return (
         <div className="ma4 mt0">
-
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 250 }} >
+            <div className="Tilt-inner"><img alt = 'logo' src={teddy}/> </div>
+            </Tilt>
         </div>
         
     );
